298d9b1e1713fd32647e1abdc43e40376c0f8a03
tpett20 <thomaspettersen20@gmail.com>
2023-05-12 10:15:48 -0400

    index page only displays searches to match logged in user, if no user, display default options.

--- src/pages/Searches/Index/index.js
+++ src/pages/Searches/Index/index.js
@@ -2,18 +2,26 @@ import { useState, useEffect } from "react"
 import { getSearches, createSearch } from '../../../utilities/searches-services'
 import ResultsCard from "../../../components/Results/ResultsCard"
 import SearchForm from "../../../components/SearchForm/SearchForm"
-import TestUser from "../../../components/Auth/TestUser"
 import { useAuth0 } from "@auth0/auth0-react"
 
 export default function SearchesIndex() {
+    
     const [searches, setSearches] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const { user, isAuthenticated, isLoading: auth0Loading } = useAuth0()
 
     async function handleRequest() {
         try {
             const apiResponse = await getSearches()
-            setSearches(apiResponse)
-            setIsLoading(false)
+            if (user) {
+                const filteredSearches = apiResponse.filter(s => s.user === user.sub)
+                setSearches(filteredSearches)
+                setIsLoading(false)
+            } else {
+                const filteredSearches = apiResponse.filter(s => s.user === undefined)
+                setSearches(filteredSearches)
+                setIsLoading(false)
+            }
         } catch (err) {
             console.log(err)
         }
@@ -22,7 +30,7 @@ export default function SearchesIndex() {
     useEffect(() => {
         handleRequest()
         // eslint-disable-next-line
-    }, [isLoading])
+    }, [isLoading, auth0Loading])
 
     const loaded = () => {
         return (
