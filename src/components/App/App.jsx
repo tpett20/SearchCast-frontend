dcf0e57b2d2d88af729147c0a5c105bb5a68b51a
tpett20 <thomaspettersen20@gmail.com>
2023-08-01 16:22:34 -0400

    correct async loading error when retreiving new Spotify Token by adding a connectionLoading variable to App.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getSpotifyToken, setSpotifyToken } from '../../utilities/spotifyToken';
 import { setSpotifyTokenTimer, checkSpotifyTokenTimer } from '../../utilities/SpotifyTokenTimer';
 import { accessSpotify } from '../../utilities/results-services';
@@ -8,6 +8,8 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 
 function App() {
+
+    const [connectionLoading, setConnectionLoading] = useState(true)
 
     const establishSpotifyConnection = async () => {
         const localToken = getSpotifyToken()
@@ -18,9 +20,12 @@ function App() {
                 const newToken = await accessSpotify()
                 setSpotifyToken(newToken)
                 setSpotifyTokenTimer()
+                setConnectionLoading(false)
             } catch (err) {
                 console.log(err)
             }
+        } else {
+            setConnectionLoading(false)
         }
     }
 
@@ -28,7 +33,7 @@ function App() {
         establishSpotifyConnection()
     }, [])
 
-    return (
+    return connectionLoading ? <></> : (
         <div className="App container">
             <Header />
             <Main />
